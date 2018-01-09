e21bed1c349626aa2657799f17dec9f2a73c0d99
kvieta <sneak@escapethebot.net>
2018-01-09 23:03:00 +0000

    bug squashing

--- curse.sh
+++ curse.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
-if [ $# -eq 0 ]
+if [ -z "$1" ]
   then
     echo "Specify a modpack zip."
     exit 1
+elif [[ "$1" == *http*zip* ]]
+  then
+    echo "Installing modpack from $1"
+    echo "Link given. Downloading modpack..."
+    wget -q --show-progress "$1"
+    zip=$(echo $1 | awk -F '/' '{ print $(NF) }' | sed 's/%20/ /g')
+elif [[ "$1" == *\.zip* ]] && [ -d $1 ]
+  then
+    echo "Installing modpack from $1"
+    echo "Local file detected."
+    zip="$1"
+else
+    echo "Don't know what to do with \"$1\", does it exist?"
+    exit 1
 fi
 
-echo "Installing modpack from $1"
-XDIR="$(echo $1 |sed 's/.zip//')"
-#echo "debug: XDIR=\"$XDIR\""
+xdir="$(echo $zip | sed 's/.zip//')"
+#echo "debug: zip=\"$zip\", xdir: \"$xdir\""
 
-unzip -q "$1" -d "$XDIR"
+
+unzip -q "$zip" -d "$xdir"
 
 #getmod $modid $fileid
 function getmod {
-#  echo "debug: $0 $1 $2 $3"
+#  echo "debug: 1: $1 2: $2"
   modurl=$(curl -A "kvieta-curse-dl" -s "https://cursemeta.dries007.net/$1/files.json" | sed 's/}/}\n/g' | grep "$2" | xargs | tr ',' '\n' | grep Download | sed 's/DownloadURL://')
 #  echo "debug: $modurl"
   wget -q --show-progress --no-clobber "$modurl"
 }
 
-cd "$XDIR"
+cd "$xdir"
 mkdir cursemods
 cd cursemods
 
 #bash is turing complete
-mod_arr=( $(grep 'projectID\|fileID' ../manifest.json | sed '1~2 s/,//' | tr '\n' ' '| xargs | tr ',' '\n' | sed 's/projectID://g;s/fileID:/,/g;s/ //g') )
+#mod_arr=( $(grep 'projectID\|fileID' ../manifest.json | sed '1~2 s/,//' | tr '\n' ' '| xargs | tr ',' '\n' | sed 's/projectID://g;s/fileID:/,/g;s/ //g') )
+mod_arr=( $(grep 'projectID\|fileID' ../manifest.json | sed '1~2 s/,//' | tr '\r' ' ' | tr ',' '\n' | xargs |sed 's/projectID://g;s/fileID:/,/g;s/ //g') )
 mod_arr_length=${#mod_arr[@]}
+
 for (( i=0; i<${mod_arr_length}+1; i++ ));
   do
     getmod $(echo ${mod_arr[$i]} | sed 's/,/ /')
@@ -49,6 +65,6 @@ if [ -d "overrides/config" ]
   else
     echo "Modpack does not provide configs"
 fi
-mv "cursemods/*" "minecraft/mods/"
+mv cursemods/* "minecraft/mods/"
 
 #todo: option to write instance.cfg and copy into multimc instance dir
